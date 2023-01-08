557c7fa51abe3b8fe5fc70dd9fe94b3006da750b
Lucas Mendes <digitalartist2013@gmail.com>
2023-01-08 14:50:51 -0300

    Versão Jogável - 1.1.0
    Adicionado sistema de inventario funcional

--- Scenes/Inventory/Inventory.gd
+++ Scenes/Inventory/Inventory.gd
@@ -6,8 +6,15 @@ var itemScene = load("res://Scenes/Inventory/Item_BT.tscn")
 
 func _ready():
 	camera = get_node(camera)
-	call_itens("Consumiveis")
-
+	call_itens("Upgrades")
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_left"):
+		for i in $BG_Inventory/GridContainer.get_children():
+			i.queue_free()
+		
+		get_itens("Orbe de Conexão", "item adicionado", "res://icon.png", "Upgrades")
+		
 func _on_BT_Inventario_pressed():
 	$BT_Inventario.hide()
 	$BG_Inventory.show()
@@ -39,6 +46,7 @@ func call_itens(type):
 			newItn.descr = allItens[0][allItens[0].keys()[i]][0]
 			newItn.icon = load(allItens[0][allItens[0].keys()[i]][1])
 			newItn.nameItem = allItens[0].keys()[i]
+			newItn.type = type
 	elif type == "Consumiveis":
 		for i in allItens[1].size():
 			var newItn = itemScene.instance()
@@ -46,8 +54,13 @@ func call_itens(type):
 			newItn.descr = allItens[1][allItens[1].keys()[i]][0]
 			newItn.icon = load(allItens[1][allItens[1].keys()[i]][1])
 			newItn.nameItem = allItens[1].keys()[i]
+			newItn.type = type
 
-
+func get_itens(nameItem,descr,icon,type):
+	if type == "Upgrades":
+		allItens[0][nameItem] = [descr,icon]
+		call_itens(type)
+		
 func _on_bt_item_down(item):
 	for i in $BG_Inventory/GridContainer.get_children():
 		i.queue_free()
--- Scenes/Inventory/Item_BT.gd
+++ Scenes/Inventory/Item_BT.gd
@@ -16,3 +16,34 @@ func _on_Item_BT_mouse_entered():
 
 func _on_Item_BT_mouse_exited():
 	descriptionBox.hide()
+
+func _on_Item_BT_pressed():
+	descriptionBox.hide()
+	if type == "Upgrades":
+		match nameItem:
+			"Disruptor de sinal":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[0].erase(nameItem)
+				queue_free()
+				print("disruptor")
+			"Raio Imobilizador":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[0].erase(nameItem)
+				queue_free()
+				print("raio")
+			"Orbe de Conexão":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[0].erase(nameItem)
+				queue_free()
+				print("conexao")
+			"Orbe de Limpeza":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[0].erase(nameItem)
+				queue_free()
+				print("limpeza")
+	elif type == "Consumiveis":
+		match nameItem:
+			"Orbe de Conexão":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[1].erase(nameItem)
+				queue_free()
+				print("conexao")
+			"Orbe de Limpeza":
+				get_tree().get_nodes_in_group("Inventory")[0].allItens[1].erase(nameItem)
+				queue_free()
+				print("limpeza")
