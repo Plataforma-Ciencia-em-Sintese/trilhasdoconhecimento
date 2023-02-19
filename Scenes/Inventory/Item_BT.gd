efa58015fe48d1c0947a89a7cdf4da3546405f33
Lucas Mendes <digitalartist2013@gmail.com>
2023-02-19 16:10:23 -0300

    Feat - Adicionado suavização nas câmeras / Inventario remodelado

--- Scenes/Inventory/Item_BT.gd
+++ Scenes/Inventory/Item_BT.gd
@@ -6,19 +6,28 @@ var descr: String
 var image: Texture
 
 var descriptionBox
+var bigDescriptionBox
 
 func _ready():
 	descriptionBox = get_tree().get_nodes_in_group("Description")[0]
+	bigDescriptionBox = get_tree().get_nodes_in_group("BigDescription")[0]
 
 func _on_Item_BT_mouse_entered():
 	descriptionBox.show()
+	bigDescriptionBox.show()
+	
 	descriptionBox.get_node("Txt").text = descr
+	bigDescriptionBox.get_node("Title").text = nameItem
+	bigDescriptionBox.get_node("Desc").text = descr
+	bigDescriptionBox.get_node("Quant").text = "Sem valor ainda"
 
 func _on_Item_BT_mouse_exited():
 	descriptionBox.hide()
+	bigDescriptionBox.hide()
 
 func _on_Item_BT_pressed():
 	descriptionBox.hide()
+	bigDescriptionBox.hide()
 	if type == "Upgrades":
 		match nameItem:
 			"Disruptor de sinal":
--- Scenes/Sci Fi Room/CameraController.gd
+++ Scenes/Sci Fi Room/CameraController.gd
@@ -1,30 +1,42 @@
 extends Camera
 
+export (NodePath) var anchorCam1
+export (NodePath) var anchorCam2
 export (NodePath) var cam1
-export (NodePath) var cam2
+var anchorGeral
 var setCam = false
+var lookToPlayer = false
+var geralLerp = false
+
+func _ready():
+	anchorCam1 = get_node(anchorCam1)
+	anchorCam2 = get_node(anchorCam2)
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("R_Click"):
-		if !setCam:
-			get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam2)
-			get_node(cam1).current = false
-			get_node(cam2).current = true
-			setCam = true
-		else:
-			get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam1)
-			get_node(cam1).current = true
-			get_node(cam2).current = false
-			setCam = false
+	if Input.is_action_just_pressed("R_Click") and !geralLerp:
+		setCam = !setCam
 	
-func _on_Area_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam2)
-		get_node(cam1).current = false
-		get_node(cam2).current = true
+	if !geralLerp:
+		if setCam:
+			global_transform.origin = lerp(global_transform.origin,anchorCam2.global_transform.origin,0.05)
+			lookToPlayer = true
+		else:
+			global_transform.origin = lerp(global_transform.origin,anchorCam1.global_transform.origin,0.05)
+			lookToPlayer = true
+	else:
+		global_transform.origin = lerp(global_transform.origin,anchorGeral.global_transform.origin,0.05)
+	
+	if lookToPlayer:
+		look_at(get_parent().global_transform.origin,Vector3.UP)
 
-func _on_Area2_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam1)
-		get_node(cam1).current = true
-		get_node(cam2).current = false
+#func _on_Area_body_entered(body):
+#	if body.is_in_group("Player"):
+#		get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam2)
+#		get_node(cam1).current = false
+#		get_node(cam2).current = true
+#
+#func _on_Area2_body_entered(body):
+#	if body.is_in_group("Player"):
+#		get_tree().get_nodes_in_group("Pointer")[0].camera = get_node(cam1)
+#		get_node(cam1).current = true
+#		get_node(cam2).current = false
