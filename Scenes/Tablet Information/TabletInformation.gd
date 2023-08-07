cff35d6175392215b34d5f4e9a14aa3406ead5d5
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-07 19:11:36 -0300

    Feat - Adicionado botões e BG da tela dos Notáveis

--- Scenes/Tablet Information/TabletInformation.gd
+++ Scenes/Tablet Information/TabletInformation.gd
@@ -15,28 +15,33 @@ func _on_BT_tablet_pressed():
 	owner.get_node("States/Talking").show()
 	
 func _on_BT_Invent_pressed():
+
 	owner.get_node("Inventory/BG_Inventory").show()
 	owner.get_node("States/Move").hide()
 	owner.get_node("States/Talking").show()
 	$PanelTablet.hide()
 	
 func _on_BT_Config_pressed():
+
 	$Scenes/Control_Options/Panel_Options.show()
 	$Scenes/Control_Options/Panel_Options/BT_ExitConfig.show()
 	$PanelTablet.hide()
 	
 
 func _on_BT_Bestiario_pressed():
+
 	$Scenes/BestiaryEnemy/Panel_Enemy.show()
 	$Scenes/BestiaryEnemy/exit_Enemy.show()
 	$PanelTablet.hide()
 
 func _on_BT_Contatos_pressed():
+
 	$Scenes/BestiaryNotables/Panel_Notables.show()
 	$Scenes/BestiaryNotables/exit_Notables.show()
 	$PanelTablet.hide()
 	
 func _on_BT_Trilhas_pressed():
+
 	$Scenes/Trails/Panel_trails.show()
 	$Scenes/Trails/exit_Trails.show()
 	$PanelTablet.hide()
