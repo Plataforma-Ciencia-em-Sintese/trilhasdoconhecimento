0390d41ac0fa4cf3a7a74a4d506ef5822d6f0847
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-10 02:17:09 -0300

    Feat - Adicionado Life e energy no tablet

--- Scenes/HUDs/Options/options_control.gd
+++ Scenes/HUDs/Options/options_control.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 onready var save_file = SaveFile.g_data
 
--- Scenes/Tablet Information/Panel_InfoPlayer_ TB.gd
+++ Scenes/Tablet Information/Panel_InfoPlayer_ TB.gd
@@ -1,16 +1,10 @@
 extends Panel
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$TP_life.value = GlobalValues.life
+	$TP_life/Label.text = str(GlobalValues.life) 
+	
+	$TP_Energy.value = GlobalValues.energy
+	$TP_Energy/Label.text = str(GlobalValues.energy) 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
