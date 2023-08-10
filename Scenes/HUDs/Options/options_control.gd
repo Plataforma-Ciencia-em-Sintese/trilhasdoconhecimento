921161508c185bcfd19cca85814fe5bca897c142
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-10 15:23:04 -0300

    Feat - Sistema de som geral do jogo funcionando
    Ajustei a barra de volume das configurações

--- Scenes/HUDs/Options/options_control.gd
+++ Scenes/HUDs/Options/options_control.gd
@@ -17,15 +17,19 @@ var busSFX: AudioBusLayout
 
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
-	$Panel_Options/Fundo/Sound/HSlider_Sound.value = save_file.saveVolume
 	
+	$Panel_Options/Fundo/Sound/HSlider_Sound.value = 10
+	Fmod.set_bus_volume(bus_path_Master, 10)
+	
+	#Parte de Save 
+	$Panel_Options/Fundo/Sound/HSlider_Sound.value = save_file.saveVolume
 
 func _on_HSlider_Sound_value_changed(value):
-	busMaster = Fmod.set_bus_volume(bus_path_Master, 1)
-	if value == -30:
-		busMaster = Fmod.set_bus_volume(bus_path_Master, 0)
+	if value == 0:
+		Fmod.set_bus_mute(bus_path_Master, true)
 	else:
-		busMaster = Fmod.set_bus_volume(bus_path_Master, 1)
+		Fmod.set_bus_mute(bus_path_Master, false)
+		Fmod.set_bus_volume(bus_path_Master, value)
 
 func _on_BT_ExitConfig_pressed():
 	save_file.saveVolume = $Panel_Options/Fundo/Sound/HSlider_Sound.value
