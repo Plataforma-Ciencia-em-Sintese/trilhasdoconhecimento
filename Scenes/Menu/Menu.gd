a6a5895f4e4425b5c1cdec81589fe234901ed10b
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-10 19:50:26 -0300

    Feat -  Ajuste para fazer a build

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -4,10 +4,10 @@ export (NodePath) var camera
 
 func _on_Play_pressed()-> void:
 	Fmod.play_one_shot("event:/SFX/Menu/BotaoIniciar", self)
-	var _play: bool = get_tree().change_scene("res://Scenes/Menu/Selection_Zone_3D.tscn")
+	get_tree().change_scene("res://Scenes/Menu/Selection_Zone_3D.tscn")
 
 func _on_Quit_pressed():
-	get_tree().quit()
+	pass
 
 
 func _on_Configure_pressed():
