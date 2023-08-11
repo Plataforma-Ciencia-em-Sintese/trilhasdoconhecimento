cf707f02572e28aea185aee928d1caeb4447da88
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-11 01:16:15 -0300

    Feat - Adicionado Design do menu

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -6,10 +6,6 @@ func _on_Play_pressed()-> void:
 	Fmod.play_one_shot("event:/SFX/Menu/BotaoIniciar", self)
 	get_tree().change_scene("res://Scenes/Menu/Selection_Zone_3D.tscn")
 
-func _on_Quit_pressed():
-	pass
-
-
 func _on_Configure_pressed():
 	$Control_Options/Panel_Options.show()
 	$Buttons/Play.hide()
@@ -18,3 +14,7 @@ func _on_Configure_pressed():
 	$NameGame.hide()
 	
 
+
+
+func _on_Sobre_pressed():
+	pass # Replace with function body.
