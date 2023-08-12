944b9753fc25e820bb9a07add2168c050f53bc70
Lucas Mendes Leite <digitalartist2013@gmail.com>
2023-08-11 21:35:15 -0300

    Feat - Criado sistema de elevador

--- Management/Sound System/Script/Music_Scene_Player.gd
+++ Management/Sound System/Script/Music_Scene_Player.gd
@@ -7,7 +7,7 @@ var stageName : String
 func _ready():
 	# Seta a cena local pro script de quest saber onde o jogador esta
 	GlobalQuest.localScene = self
-	# Confere se a cena participa de alguma quest pelo script global quest
+	# Confere se a cena participa de alguma quest
 	stageName = get_tree().get_current_scene().get_name()
 	if GlobalQuest.questResource != null:
 		print(get_tree().get_current_scene().get_name())
---
+++ Scenes/Elevator/Elevator_Controller.gd
@@ -0,0 +1,41 @@
+extends Area
+
+# Fases pre dipsostas para selecionar
+export (String,"Cyberspace","Primeiro Andar","Segundo Andar") var sceneToGo
+# Cenas disponiveis
+# A ordem da array e manual
+export (Array,String) var scenes
+# Serve para bloquear a entrada white
+# A cena e global white transition
+export (bool) var activatedFade
+# Resource contendo os efeitos sonoros do inventario
+export (Resource) var sfxResource
+
+func _ready():
+	if activatedFade:
+		WhiteTransition.start_transition("fadeout")
+
+func _on_Elevator_Controller_body_entered(body):
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("Open")
+		play_sfx("play_one",sfxResource.sfx["PortaAutomaticaAbrindo"])
+		# Trava o pointer dentro do elevador pro player ir ate la
+		var pointer = get_tree().get_nodes_in_group("Pointer")[0]
+		pointer.global_transform.origin = $Model.global_transform.origin
+		pointer.isStopped = true
+		yield(get_tree().create_timer(1),"timeout")
+		$AnimationPlayer.play("Close")
+		play_sfx("play_one",sfxResource.sfx["PortaAutomaticaFechando"])
+		WhiteTransition.start_transition("fadein")
+		yield(get_tree().create_timer(2),"timeout")
+		match sceneToGo:
+			"Cyberspace":
+				get_tree().change_scene_to(load(scenes[0]))
+			"Primeiro Andar":
+				get_tree().change_scene_to(load(scenes[1]))
+			"Segundo Andar":
+				get_tree().change_scene_to(load(scenes[2]))
+
+func play_sfx(event,name):
+	if name:
+		GlobalMusicPlayer.play_sound(event,name)
