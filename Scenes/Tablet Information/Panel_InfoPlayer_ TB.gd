df143ae47ebf0b957f28a48675f08cd18931fc47
Naju.Oliveira <a.julia.barbosa@outlook.com>
2023-08-14 19:02:22 -0300

    Fix - ajustado valor da fonte do inventario

--- Scenes/Tablet Information/Panel_InfoPlayer_ TB.gd
+++ Scenes/Tablet Information/Panel_InfoPlayer_ TB.gd
@@ -2,11 +2,11 @@ extends Panel
 
 
 func _ready():
-	$TP_life.value = GlobalValues.life
-	$TP_life/Label.text = str(GlobalValues.life) 
+	$TP_life.value = GlobalValues.lifeActual
+	$TP_life/Label.text = str(GlobalValues.lifeActual) 
 	
-	$TP_Energy.value = GlobalValues.energy
-	$TP_Energy/Label.text = str(GlobalValues.energy) 
+	$TP_Energy.value = GlobalValues.energyActual
+	$TP_Energy/Label.text = str(GlobalValues.energyActual) 
 	
 	$TP_XP.value = GlobalValues.xpActual
 	$TP_XP/Label.text = str(GlobalValues.xpActual) 
