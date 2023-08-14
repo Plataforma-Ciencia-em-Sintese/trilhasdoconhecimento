1d0f33ad0629c89ff83a7f5bfbb40c9fe7e9498a
Lucas Mendes Leite <digitalartist2013@gmail.com>
2023-08-14 18:17:24 -0300

    Feat - Criaçao da tela de patrocinadores e progressão funcional

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -2,6 +2,9 @@ extends Control
 
 export (NodePath) var camera
 
+func _ready():
+	WhiteTransition.start_transition("fadeout")
+
 func _on_Play_pressed()-> void:
 	Fmod.play_one_shot("event:/SFX/Menu/BotaoIniciar", self)
 	get_tree().change_scene("res://Scenes/Menu/Selection_Zone_3D.tscn")
@@ -10,9 +13,8 @@ func _on_Configure_pressed():
 	$Control_Options/Panel_Options.show()
 	$Buttons/Play.hide()
 	$Buttons/Configure.hide()
-	$Buttons/Quit.hide()
+#	$Buttons/Quit.hide()
 	$NameGame.hide()
 	
-
 func _on_Sobre_pressed():
 	pass # Replace with function body.
